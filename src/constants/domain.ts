517893c010a4d74110881a0d4f0223cfdac4b870
namdev <namnkq2603@gmail.com>
2025-03-21 21:51:01 +0700

    fix: update whitelist domains

--- src/constants/domain.ts
+++ src/constants/domain.ts
@@ -2,4 +2,5 @@ export const WHITELIST_DOMAINS: string[] = [
   // 'http://localhost:3000',
   // 'http://localhost:5173',
   // 'http://localhost:8017'
+  'https://ecommerce-server-e2nz.onrender.com'
 ]
