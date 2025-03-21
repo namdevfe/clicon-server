aaf2ec0ef5c8ef9cfe4c7df78efcd73b053c6beb
namdev <namnkq2603@gmail.com>
2025-03-21 15:48:36 +0700

    fix: remove register on userController.ts file

--- src/controllers/userController.ts
+++ src/controllers/userController.ts
@@ -1,15 +1,6 @@
 import { NextFunction, Request, Response } from 'express'
 import userService from '~/services/userService'
 
-const register = async (req: Request, res: Response, next: NextFunction) => {
-  try {
-    const registeredUser = await userService.register(req.body)
-    res.status(registeredUser.statusCode).json(registeredUser)
-  } catch (error) {
-    next(error)
-  }
-}
-
 const getList = async (req: Request, res: Response, next: NextFunction) => {
   try {
     const usersRes = await userService.getList(req.query)
@@ -40,7 +31,6 @@ const deleteById = async (req: Request, res: Response, next: NextFunction) => {
 }
 
 const userController = {
-  register,
   getList,
   updateById,
   deleteById
