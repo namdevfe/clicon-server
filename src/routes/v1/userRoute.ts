b2607f294ce44dd51f8df82bf29ffa29095d974d
namdev <namnkq2603@gmail.com>
2025-03-30 18:00:02 +0700

    docs: update users api on swagger

--- src/routes/v1/userRoute.ts
+++ src/routes/v1/userRoute.ts
@@ -4,6 +4,83 @@ import userValidation from '~/validations/userValidation'
 
 const router: Router = express.Router()
 
+/**
+ * @swagger
+ * /user/add-user:
+ *   post:
+ *     tags:
+ *       - Users
+ *     summary: Add user by admin
+ *     description: Add user by admin with status inactive and send OTP code with email address to verify emaill
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               email:
+ *                 type: string
+ *               password:
+ *                 type: string
+ *               firstName:
+ *                 type: string
+ *               lastName:
+ *                 type: string
+ *     responses:
+ *        200:
+ *         description: Return message send OTP success.
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 statusCode:
+ *                   type: number
+ *                   example: 200
+ *                 message:
+ *                   type: string
+ *                   example: Send mail is successfully
+ *        422:
+ *         description: Return error uncessable entity.
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 statusCode:
+ *                   type: number
+ *                   example: 422
+ *                 message:
+ *                   type: string
+ *                   example: Invalid email
+ *        400:
+ *         description: Return error message to notify this email had already been used.
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 statusCode:
+ *                   type: number
+ *                   example: 400
+ *                 message:
+ *                   type: string
+ *                   example: Email had already been used
+ *        500:
+ *          description: Return internal server error.
+ *          content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 statusCode:
+ *                   type: number
+ *                   example: 500
+ *                 message:
+ *                   type: string
+ *                   example: Internal Server Error
+ */
 router.post('/add-user', userValidation.addUser, userController.addUser)
 
 export default router
